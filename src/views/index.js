6ee799b7ed7e707be05db012d0e18c0031cb05b5
Stu Thompson <stuart@stuartthompson.net>
2018-11-22 14:32:57 -0800

    Learning about styles with flexbox

--- src/views/index.js
+++ src/views/index.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import HomeView from './HomeView';
 import GameListView from './GameListView';
 import GameDetailView from './GameDetailView';
+import TopNavigation from '../components/navigation/TopNavigation';
 
 const layoutView = (ContentView) => props => (
     <div>
-        <span>Top Navigation</span>
+        <TopNavigation />
         <div>
             <ContentView {...props} />
         </div>
