e3145f234dcab23b99a9c364a932b549e65e8582
Stu Thompson <stuart@stuartthompson.net>
2018-11-23 10:03:46 -0800

    Background color for menu as a prop.

---
+++ src/components/navigation/Menu.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const styles = {
+	menuContainer: {
+        backgroundColor: '#00ff00',
+		display: 'flex',
+		flex: 1,
+		flexDirection: 'row',
+		height: 48,
+		alignItems: 'center',
+		textAlign: 'center'
+	},
+	menuItemContainer: {
+		backgroundColor: '#555555',
+		color: '#000000',
+		display: 'flex',
+		flexDirection: 'column',
+		height: '100%',
+		textDecoration: 'none'
+	},
+	menuItemSpacer: {
+		flex: 1
+	},
+	menuItemContentContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center'
+	},
+	menuItemIconContainer: {},
+	menuItemTextContainer: {
+		paddingLeft: 6,
+		textAlign: 'left'
+	}
+};
+
+/**
+ * Represents a menu component.
+ */
+class Menu extends React.Component {
+	render() {
+        const { backgroundColor, menuItems } = this.props;
+        // Calculate menu item widths
+		const totalMenuItems = menuItems.leftSide.length + menuItems.rightSide.length;
+		const menuItemWidth = 1 / totalMenuItems;
+        const spacerWidth = (totalMenuItems / 2) + 2;
+        // Map left-side menu items
+		const leftMenuItems = menuItems.leftSide.map((menuItem) => (
+			<MenuItem linkText={menuItem.linkText} linkTo={menuItem.linkTo} width={menuItemWidth} />
+        ));
+        // Map right-side menu items
+		const rightMenuItems = menuItems.rightSide.map((menuItem) => (
+			<MenuItem linkText={menuItem.linkText} linkTo={menuItem.linkTo} width={menuItemWidth} />
+		));
+		return (
+			<div style={{...styles.menuContainer, backgroundColor: backgroundColor}}>
+				{leftMenuItems}
+				<div style={{flex: spacerWidth / totalMenuItems }} />
+				{rightMenuItems}
+			</div>
+		);
+	}
+}
+
+/**
+ * Represents a menu item component.
+ */
+class MenuItem extends React.Component {
+	render() {
+		const { linkText, linkTo, width } = this.props;
+		return (
+			<Link to={linkTo} style={{...styles.menuItemContainer, flex: width}}>
+				<div style={styles.menuItemSpacer} />
+				<div style={styles.menuItemContentContainer}>
+					<div>I</div>
+					<div style={styles.menuItemTextContainer}>
+						<span>{linkText}</span>
+					</div>
+				</div>
+				<div style={styles.menuItemSpacer} />
+			</Link>
+		);
+	}
+}
+
+export default Menu;
--- src/components/navigation/TopNavigation.js
+++ src/components/navigation/TopNavigation.js
@@ -1,84 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-const styles = {
-	menuContainer: {
-		backgroundColor: '#a8a8a8',
-		display: 'flex',
-		flex: 1,
-		flexDirection: 'row',
-		height: 48,
-		alignItems: 'center',
-		textAlign: 'center'
-	},
-	menuItemContainer: {
-		backgroundColor: '#b9b9b9',
-		color: '#000000',
-		display: 'flex',
-		flexDirection: 'column',
-		height: '100%',
-		textDecoration: 'none'
-	},
-	menuItemSpacer: {
-		flex: 1
-	},
-	menuItemContentContainer: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'center'
-	},
-	menuItemIconContainer: {},
-	menuItemTextContainer: {
-		paddingLeft: 6,
-		textAlign: 'left'
-	}
-};
-
-/**
- * Represents a menu component.
- */
-class Menu extends React.Component {
-	render() {
-		const { menuItems } = this.props;
-		const totalMenuItems = menuItems.leftSide.length + menuItems.rightSide.length;
-		const menuItemWidth = 1 / totalMenuItems;
-		const spacerWidth = (totalMenuItems / 2) + 2;
-		const leftMenuItems = menuItems.leftSide.map((menuItem) => (
-			<MenuItem linkText={menuItem.linkText} linkTo={menuItem.linkTo} width={menuItemWidth} />
-		));
-		const rightMenuItems = menuItems.rightSide.map((menuItem) => (
-			<MenuItem linkText={menuItem.linkText} linkTo={menuItem.linkTo} width={menuItemWidth} />
-		));
-		return (
-			<div style={styles.menuContainer}>
-				{leftMenuItems}
-				<div style={{flex: spacerWidth / totalMenuItems }} />
-				{rightMenuItems}
-			</div>
-		);
-	}
-}
-
-/**
- * Represents a menu item component.
- */
-class MenuItem extends React.Component {
-	render() {
-		const { linkText, linkTo, width } = this.props;
-		return (
-			<Link to={linkTo} style={{...styles.menuItemContainer, flex: width}}>
-				<div style={styles.menuItemSpacer} />
-				<div style={styles.menuItemContentContainer}>
-					<div>I</div>
-					<div style={styles.menuItemTextContainer}>
-						<span>{linkText}</span>
-					</div>
-				</div>
-				<div style={styles.menuItemSpacer} />
-			</Link>
-		);
-	}
-}
+import Menu from './Menu';
 
 /**
  * Represents a top navigation bar component.
@@ -93,8 +14,9 @@ export default class TopNavigation extends React.Component {
 			],
 			rightSide: [ { linkText: 'LogOut', linkTo: 'logout' } ]
 		};
+		const backgroundColor = '#b9b9b9';
 		return (
-			<Menu menuItems={menuItems} />
+			<Menu backgroundColor={backgroundColor} menuItems={menuItems} />
 		);
 	}
 }
